31d8293a9fdf26e7b25592d517d2a5ee5637bfb5
Tylor <tylorlouis@ucla.edu>
2014-04-13 02:27:52 -0700

    Change configuration for server

--- server/globals.js
+++ server/globals.js
@@ -3,3 +3,11 @@ global.API_CORE = __dirname + '/lib/API.js';
 global.MODEL_PATH = __dirname + '/models/';
 global.CACHE = {};
 global.CONFIG = require( './config.js' );
+
+
+try {
+	global.CONFIG = require( './server.config.js' );
+}
+catch( e ) {
+	global.CONFIG = require( './config.js' );
+}
